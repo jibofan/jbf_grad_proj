1756070164afa11508276360f21165454f04069a
jibofan <jbf20031221@mail.ustc.edu.cn>
2026-05-03 20:56:28 +0800

    update thesis

--- src/draw_mat.m
+++ src/draw_mat.m
@@ -12,9 +12,13 @@ p=amd(A_0);
 % Ap = A(perm, perm);
 A_0p = A_0(perm, perm);
 
+A_0p = A_0p + 100000 * speye(size(A_0p));
+
+L = chol(A_0p);
+nnz(L)
 
 figure;
-spy(A_0);
+spy(A_0p);
 title('Sparsity Pattern');
 xlabel('Column Index');
 ylabel('Row Index');
